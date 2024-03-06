070adc2dd03f70f5f0c29df06809f9c05bcd260d
iamshubhamk <shubham.kr.735@gmail.com>
2024-03-07 00:03:45 +0530

    created_the_snake

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+starting_positions = [(0,0),(-20,0),(-40,0)]
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+screen.exitonclick()
