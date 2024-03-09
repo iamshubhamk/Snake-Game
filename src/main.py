a3cba9d9afbb3db1c9baadd3707d6f306d2b6107
iamshubhamk <shubham.kr.735@gmail.com>
2024-03-09 23:42:33 +0530

    added a food on the screen and a scoreboard also the game over feature.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,6 +25,12 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food) < 15:
+        scoreboard.increase_score()
+        food.refresh()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 screen.exitonclick()
