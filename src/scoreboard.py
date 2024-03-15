39e07551a7a33b1619a076ad113b590c86b7a64c
iamshubhamk <shubham.kr.735@gmail.com>
2024-03-15 22:00:47 +0530

    added feature to keep track of the high score

--- src/scoreboard.py
+++ src/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -15,13 +17,20 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
     
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
     
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over", align=ALIGNMENT, font= FONT)
+    def write_high_score(self,high_score):
+        with open("data.txt", mode="w") as data:
+            data.write(f"{self.high_score}")
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_high_score(self.score)
+        self.score = 0
+        self.update_scoreboard()
     
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
--- src/snake.py
+++ src/snake.py
@@ -26,7 +26,13 @@ class Snake:
     
     def extend(self):
         self.add_segment(self.segments[-1].position())
-
+    
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
     
     def move(self):
         for seg_num in range(len(self.segments)-1,0,-1):
